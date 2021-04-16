315f42b22fe8483c976bbecb8146d67c16322a06
jmjos <joseph@ice.rwth-aachen.de>
2021-04-16 08:40:09 +0200

    Slides

---
+++ tutorial2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone https://github.com/scalesim-project/scale-sim-v2.git
+cd scale-sim-v2
+git checkout tutorial/asplos/tutorial2
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python setup.py install
